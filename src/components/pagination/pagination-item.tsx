c6696b634bc7555b300cfa190cc994635279de04
LaamGinghong <351390560@qq.com>
2020-02-28 16:26:59 +0800

    pagination

---
+++ src/components/pagination/pagination-item.tsx
@@ -0,0 +1,16 @@
+import React, { FC, HTMLAttributes } from 'react'
+import classNames from 'classnames'
+
+const PaginationItem: FC<HTMLAttributes<HTMLDivElement>> = ({
+  children,
+  className,
+  ...rest
+}) => {
+  return (
+    <div {...rest} className={classNames('pagination-item', className)}>
+      {children}
+    </div>
+  )
+}
+
+export default PaginationItem
